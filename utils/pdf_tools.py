c8c6fe1432657bd8b29bf4f3bdae3c28a0642465
VNTime JSC <vntime.jsc@gmail.com>
2025-12-26 23:08:28 +0700

    auto: v4.2.27 - fix: Add fallback stubs for pdf_tools import errors

--- utils/pdf_tools.py
+++ utils/pdf_tools.py
@@ -47,6 +47,56 @@ except ImportError as e:
         HAS_PYMUPDF = True
     except ImportError:
         pass
+    
+    # Provide stub functions for backward compatibility
+    def merge_pdfs(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def split_pdf(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def compress_pdf(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def protect_pdf(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def post_process_pdf(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def rasterize_pdf(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def add_watermark(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def pdf_to_images(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def images_to_pdf(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def parse_page_range(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def extract_pages(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def extract_pdf_pages(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def delete_pages(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def rotate_pages(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def reorder_pages(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+    
+    def reverse_pages(*args, **kwargs):
+        raise NotImplementedError("PyMuPDF not available")
+
 
 
 # Make all functions available for import

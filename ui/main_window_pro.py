708677f65b4e8d2d42c3fb6bc7e2d4c0bed10d7d
VNTime JSC <vntime.jsc@gmail.com>
2025-12-26 23:52:20 +0700

    auto: v4.2.36 - docs: Complete v3.0 bug fixing and stability report

--- ui/main_window_pro.py
+++ ui/main_window_pro.py
@@ -758,7 +758,7 @@ class FileListPanel(ctk.CTkFrame):
 class ConverterProApp(TkDnDWrapper):
     """Professional-grade Office to PDF Converter with robust Unicode drag-and-drop support."""
 
-    VERSION = "4.2.35"
+    VERSION = "4.2.36"
 
     def __init__(self):
         super().__init__()
